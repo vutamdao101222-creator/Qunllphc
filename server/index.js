d1947e8015cde58c2a293cd2f68b9787cf5b476f
vutamdao101222-creator <vutamdao101222@gmail.com>
2026-05-07 23:25:50 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,17 +2,29 @@ import { env } from './config/env.js';
 import { createApp } from './app.js';
 import { startSimulationJob } from './jobs/simulationJob.js';
 import { runMigrations } from './migrations/runMigrations.js';
-import { logInfo } from './utils/logger.js';
+import { logError, logInfo } from './utils/logger.js';
 
 async function main() {
-  await runMigrations();
   const app = createApp();
 
   app.listen(env.apiPort, () => {
     logInfo('API started', { port: env.apiPort, baseUrl: `http://localhost:${env.apiPort}` });
   });
 
-  startSimulationJob();
+  try {
+    await runMigrations();
+  } catch (err) {
+    logError(
+      'DB init/migrations failed. API will start, but DB-backed endpoints may fail. Check DB_* env vars (DB_USER/DB_PASSWORD) or set DB_AUTO_MIGRATE=false.',
+      err,
+    );
+  }
+
+  try {
+    startSimulationJob();
+  } catch (err) {
+    logError('Simulation job failed to start', err);
+  }
 }
 
 main();
